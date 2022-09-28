f192d3699ce985f66c987f7a64ffd048082d9270
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-28 09:33:11 +0530

    Created about area

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -12,6 +12,21 @@ function About(){
                 <img src={aboutBG} alt="" />
             </div>
             <div className="_textArea">
+                <div className="_name">
+                    <h1>I'm NagaSivaji</h1>
+                </div>
+                <div className="_list">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti at, voluptatem mollitia maxime in blanditiis? Quaerat sed quis aut voluptate nulla unde sit? Non laborum ut quisquam, ipsam fugit amet!</li>
+                        <li>consectetur adipisicing elit. </li>
+                        <li>consectetur adipisicing elit. </li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti at, voluptatem mollitia maxime in blanditiis? Quaerat sed quis aut voluptate nulla unde sit? Non laborum ut quisquam, ipsam fugit amet!</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
+                        
+                    </ul>
+                </div>
             </div>
         </div>
     );
